a8e13e84ce0b2e700e81e0bd5a1a59f2de0184c2
Fullooh <angstdemon1@aol.com>
2024-11-10 21:15:51 -0500

    Updated Background and Wigdets added a leads page and back button

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -26,33 +26,12 @@ const Sidebar = () => {
                 aria-hidden="true"
                 className="w-5 h-5"
               >
-                {/* SVG path for Dashboard Icon */}
+                <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8v-10h-8v10zm0-18v6h8V3h-8z" />
               </svg>
             </div>
             Dashboard
           </div>
         </Link>
-
-        <Link href="/ecommerce">
-          <div
-            role="button"
-            className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 cursor-pointer"
-          >
-            <div className="grid mr-4 place-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                aria-hidden="true"
-                className="w-5 h-5"
-              >
-                {/* SVG path for E-Commerce Icon */}
-              </svg>
-            </div>
-            E-Commerce
-          </div>
-        </Link>
-
         {/* Add more links similarly */}
       </nav>
 
