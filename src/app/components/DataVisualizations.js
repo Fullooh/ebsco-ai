eb395a210c86c6f93dae64878d24259597eb7407
Alicia Yu <aliciayu@Alicias-MacBook-Air.local>
2024-11-17 23:16:39 -0800

    Refactor DataVisualizations component layout to include border

--- src/app/components/DataVisualizations.js
+++ src/app/components/DataVisualizations.js
@@ -26,30 +26,32 @@ export default function DataVisualizations() {
     }, []); 
 
     return (
-        <div className="tableauPlaceholder" id="viz1731912329724" style={{ position: 'relative' }}>
-            <noscript>
-                <a href="#">
-                    <img
-                        alt="Dashboard 1"
-                        src="https://public.tableau.com/static/images/4Q/4QD3YTFDY/1_rss.png"
-                        style={{ border: 'none' }}
-                    />
-                </a>
-            </noscript>
-            <object className="tableauViz" style={{ display: 'none' }}>
-                <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
-                <param name="embed_code_version" value="3" />
-                <param name="path" value="shared/4QD3YTFDY" />
-                <param name="toolbar" value="yes" />
-                <param name="static_image" value="https://public.tableau.com/static/images/4Q/4QD3YTFDY/1.png" />
-                <param name="animate_transition" value="yes" />
-                <param name="display_static_image" value="yes" />
-                <param name="display_spinner" value="yes" />
-                <param name="display_overlay" value="yes" />
-                <param name="display_count" value="yes" />
-                <param name="language" value="en-US" />
-                <param name="filter" value="publish=yes" />
-            </object>
+        <div className="bg-white p-6 rounded-lg shadow ring-1 ring-gray-300">
+            <div className="tableauPlaceholder" id="viz1731912329724" style={{ position: 'relative' }}>
+                <noscript>
+                    <a href="#">
+                        <img
+                            alt="Dashboard 1"
+                            src="https://public.tableau.com/static/images/4Q/4QD3YTFDY/1_rss.png"
+                            style={{ border: 'none' }}
+                        />
+                    </a>
+                </noscript>
+                <object className="tableauViz" style={{ display: 'none' }}>
+                    <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
+                    <param name="embed_code_version" value="3" />
+                    <param name="path" value="shared/4QD3YTFDY" />
+                    <param name="toolbar" value="yes" />
+                    <param name="static_image" value="https://public.tableau.com/static/images/4Q/4QD3YTFDY/1.png" />
+                    <param name="animate_transition" value="yes" />
+                    <param name="display_static_image" value="yes" />
+                    <param name="display_spinner" value="yes" />
+                    <param name="display_overlay" value="yes" />
+                    <param name="display_count" value="yes" />
+                    <param name="language" value="en-US" />
+                    <param name="filter" value="publish=yes" />
+                </object>
+            </div>
         </div>
     );
 }
